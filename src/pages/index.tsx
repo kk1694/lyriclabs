c547aac9c101cdc4fbc04c27238f35477cf0d785
krisztian <krisztiankovacs@fastmail.com>
2023-03-30 09:32:01 +0100

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import Head from "next/head";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
+
+  const { data } = api.poems.getAll.useQuery();
 
   return (
     <>
@@ -19,6 +19,14 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {user.isSignedIn && <SignOutButton />}
         {!user.isSignedIn && <SignInButton />}
+        <div>
+          {data?.map((p) => (
+            <div key={p.id}>
+              <div>{p.title}</div>
+              <div>{p.content}</div>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
