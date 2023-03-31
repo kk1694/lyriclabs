0e86871ba2505a8ff939f6aee90786972dda1d87
krisztian <krisztiankovacs@fastmail.com>
2023-03-31 10:23:03 +0100

    commit

--- src/server/api/routers/poems.ts
+++ src/server/api/routers/poems.ts
@@ -1,9 +1,18 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { privateProcedure } from "../trpc";
 
 export const poemRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.poem.findMany();
+  }),
+  fromUser: privateProcedure.query(({ ctx }) => {
+    return ctx.prisma.poem.findMany({
+      where: {
+        authorId: ctx.userId,
+      },
+      orderBy: [{ createdAt: "desc" }],
+    });
   }),
 });
